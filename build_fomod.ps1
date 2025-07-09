88c1d81a9f9970825b2c185d0a788c731aff113e
strangerism <massimovirgilio@hotmail.com>
2025-07-09 08:14:03 +0200

    first commit

---
+++ build_fomod.ps1
@@ -0,0 +1,29 @@
+
+
+function CreateBuildFolders {
+    
+    New-Item -Path "build" -ItemType Directory | Out-Null
+
+    New-Item -Path "build/NPE - GAMMA EXP Edition Redux" -ItemType Directory | Out-Null
+    
+}
+
+function BuildMod {
+
+    $target = "build/NPE - GAMMA EXP Edition Redux"
+    Copy-Item -Recurse -Force -Path  ".\Main\gamedata" -Destination $target -Exclude .bak
+
+    # Copy-Item -Force -Path ".\doc\NPE.pdf" -Destination $target
+}
+
+CreateBuildFolders
+BuildMod
+
+$compress = @{
+    Path = "build/NPE - GAMMA EXP Edition Redux/gamedata" 
+    CompressionLevel = "Fastest"
+    DestinationPath = "release/NPE GAMMA EXP Edition Redux.zip"
+}
+Compress-Archive @compress -Force
+
+Remove-Item -Force -Recurse -Path "./build"
